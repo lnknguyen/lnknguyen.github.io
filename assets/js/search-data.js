83d62695f875cd57a07497810dcda0998ad4564a
lnknguyen <lnknguyen@users.noreply.github.com>
2025-07-26 18:57:10 +0000

    Deploying to gh-pages from @ lnknguyen/lnknguyen.github.io@856521095673b3ad80303316a4afae33af6b3613 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -112,6 +112,13 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/khoinguyenluongnguyen", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
